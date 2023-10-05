d871276ba7bba18cfdf191e8b1dd426677bb9f50
GammachisT <gammachistesfaye@gmail.com>
2023-10-05 06:40:05 -0700

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
-if __name__ != "__main__":
-    exit()
-
-argStr = "{:d} argument"
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
