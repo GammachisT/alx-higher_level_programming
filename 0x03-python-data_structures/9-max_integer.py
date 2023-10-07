5907a2c67005061f66ce7e1ccf2362023222a656
GammachisT <gammachistesfaye@gmail.com>
2023-10-06 22:15:38 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    if my_list:
+        max = my_list[0]
+        for num in my_list:
+            if num >= max:
+                max = num
+        return max
+    else:
+        return None
