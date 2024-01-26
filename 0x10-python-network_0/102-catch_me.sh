e4f53cfc11beb8c0409e46c6e15e1822c09a72a1
Gemechu T <gammachistesfaye@gmail.com>
2024-01-26 14:10:59 +0300

    Python Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that was a fun effort in breaking to http protocols on holberton servers
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You find me!" 0.0.0.0:5000/catch_me
