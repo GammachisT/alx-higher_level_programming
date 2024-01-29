010af04dc1f3280aad105450b453d9415e9f7a57
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 09:20:57 +0000

    py net

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./5-hbtn_header.py <URL>
 """
-Script that fetches https://intranet.hbtn.io/status
-"""
+import sys
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
     r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print(r.headers.get("X-Request-Id"))
