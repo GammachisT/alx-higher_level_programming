574ccd0e611b261f32c4ed41a0d6cad353c4cd65
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 08:21:19 +0000

    Py network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL and an email address,
-sends a POST request to the passed URL
-with the email as a parameter,
-and finally displays the body of the response
-"""
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response."""
+
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)\n
+if __name__ == "__main__":
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
+
