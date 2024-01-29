1799dde0714f3dbd9196351321f867d0ca67fa59
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 09:08:04 +0000

    Py network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Python script that takes GitHub credentials (username and
-password) and uses the GitHub API to display the user id."""
-
+"""
+Python script that takes 2 arguments in order to solve the given challenge
+"""
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    auth = (argv[1], argv[2])
-    request = requests.get("https://api.github.com/user", auth=auth)
-    print(request.json().get("id"))
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
