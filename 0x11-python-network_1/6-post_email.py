2ac5c1c311dc431d1c0774682b43de1cc6c4e7df
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 08:31:02 +0000

    Py network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,4 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    print(requests.post(argv[1], data={'email': argv[2]}).text, end=' ')
-
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
