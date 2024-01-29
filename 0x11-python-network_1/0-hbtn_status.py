acfa276fa473a7e377bdf336cc649cf27ed8a982
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 08:02:57 +0000

    python net #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""This script fetches data from
-    https://alx-intranet.hbtn.io/status
-"""
+"""Python script that fetches https://intranet.hbtn.io/status."""
 
-import urllib
+from urllib import request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
