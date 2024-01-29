56d391fa66cdf723788094e75272b8886012c46b
GammachiT <gammachistesfaye@gmail.com>
2024-01-29 08:41:38 +0000

    Py network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python3
-"""
-Python script that takes 2 arguments in order to solve the given challenge
-"""
+
+n script that lists list 10 commits (from the most recent
+to oldest) of the repository “rails” by the user “rails”."""
+
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
